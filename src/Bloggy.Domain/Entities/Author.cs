f19bec924632717c71048c03577c8ea0f16396df
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-10-13 14:24:34 +0300

    removed ravebdb identity proj

--- src/Bloggy.Domain/Entities/Author.cs
+++ src/Bloggy.Domain/Entities/Author.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Bloggy.Domain.Entities
 {
     public class Author
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
         public string TwitterHandle { get; set; }
