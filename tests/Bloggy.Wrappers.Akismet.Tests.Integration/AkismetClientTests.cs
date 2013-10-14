60ee54d82ab1c4f401a8bdce3895fa111d5ad829
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-10-14 23:44:23 +0300

    added CheckCommentAsyncShouldReturnSpamForASpamComment test and it passes

--- tests/Bloggy.Wrappers.Akismet.Tests.Integration/AkismetClientTests.cs
+++ tests/Bloggy.Wrappers.Akismet.Tests.Integration/AkismetClientTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -17,25 +18,58 @@ namespace Bloggy.Wrappers.Akismet.Tests.Integration
             // Arrange
             string apiKey = ConfigurationManager.AppSettings["akismet:apiKey"];
             string blog = ConfigurationManager.AppSettings["akismet:blog"];
-            AkismetClient akismetClient = new AkismetClient(apiKey, blog);
-            CommentRequestModel requestModel = new CommentRequestModel
+            using (AkismetClient akismetClient = new AkismetClient(apiKey, blog))
             {
-                UserIp = "127.0.0.1",
-                UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6",
-                Referrer = "http://www.google.com",
-                Permalink = "http://yourblogdomainname.com/blog/post=1",
-                CommentType = "comment",
-                CommentAuthor = "admin",
-                CommentAuthorEmail = "test@test.com",
-                CommentAuthorUrl = "http://www.CheckOutMyCoolSite.com",
-                CommentContent = "It means a lot that you would take the time to review our software.  Thanks again."
-            };
+                CommentRequestModel requestModel = new CommentRequestModel
+                {
+                    UserIp = "127.0.0.1",
+                    UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6",
+                    Referrer = "http://www.google.com",
+                    Permalink = "http://www.tugberkugurlu.com/blog/post=1",
+                    CommentType = "comment",
+                    CommentAuthor = "Tugberk",
+                    CommentAuthorEmail = "tugberk@tugberkugurlu.com",
+                    CommentAuthorUrl = "http://tugberk.me",
+                    CommentContent = "What do you mean by this? How can we integrate this into our pojects?"
+                };
 
-            // Act
-            AkismetResponse<bool> response = await akismetClient.CheckCommentAsync(requestModel);
+                // Act
+                AkismetResponse<bool> response = await akismetClient.CheckCommentAsync(requestModel);
 
-            // Assert
-            Assert.Equal(true, response.Entity);
+                // Assert
+                Assert.Equal(true, response.IsSuccessStatusCode);
+                Assert.Equal(false, response.Entity);
+            }
+        }
+
+        [Fact]
+        public async Task CheckCommentAsyncShouldReturnSpamForASpamComment()
+        {
+            // Arrange
+            string apiKey = ConfigurationManager.AppSettings["akismet:apiKey"];
+            string blog = ConfigurationManager.AppSettings["akismet:blog"];
+            using (AkismetClient akismetClient = new AkismetClient(apiKey, blog))
+            {
+                CommentRequestModel requestModel = new CommentRequestModel
+                {
+                    UserIp = "127.0.0.1",
+                    UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6",
+                    Referrer = "http://www.google.com/",
+                    Permalink = "http://yourblogdomainname.com/blog/post=1",
+                    CommentType = "comment",
+                    CommentAuthor = "best viagra site",
+                    CommentAuthorEmail = "theradkes@sbcglobal.net",
+                    CommentAuthorUrl = "http://bestedpillsonline.net/",
+                    CommentContent = "That's an ingenious way of thinking about it."
+                };
+
+                // Act
+                AkismetResponse<bool> response = await akismetClient.CheckCommentAsync(requestModel);
+
+                // Assert
+                Assert.Equal(true, response.IsSuccessStatusCode);
+                Assert.Equal(true, response.Entity);
+            }
         }
     }
 }
