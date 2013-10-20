66881d58e5ff305cdbfe760e9ea4a570d44a8dcb
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-10-20 14:02:24 +0300

    added RssFeedResult

---
+++ src/Bloggy.Client.Web/Infrastructure/ActionResults/FeedResult.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace Bloggy.Client.Web.Infrastructure.ActionResults
+{
+    /// <remarks>
+    /// Refer to https://github.com/benfoster/Fabrik.Common/blob/dev/src/Fabrik.Common.Web/ActionResults/FeedResult.cs.
+    /// </remarks>    
+    public class FeedResult : ActionResult
+    {
+        public SyndicationFeedFormatter Formatter { get; private set; }
+        public string ContentType { get; private set; }
+        public Encoding Encoding { get; set; }
+
+        public FeedResult(SyndicationFeedFormatter formatter, string contentType)
+        {
+            if (formatter == null)
+            {
+                throw new ArgumentNullException("formatter");
+            }
+
+            if (contentType == null)
+            {
+                throw new ArgumentNullException("contentType");
+            }
+
+            Formatter = formatter;
+            ContentType = contentType;
+            Encoding = Encoding.UTF8;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType = ContentType;
+            response.ContentEncoding = Encoding;
+
+            using (XmlTextWriter writer = new XmlTextWriter(response.Output))
+            {
+                writer.Formatting = Formatting.Indented;
+                Formatter.WriteTo(writer);
+            }
+        }
+    }
+}
---
+++ src/Bloggy.Client.Web/Infrastructure/ActionResults/RssFeedResult.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel.Syndication;
+
+namespace Bloggy.Client.Web.Infrastructure.ActionResults
+{
+    /// <summary>
+    /// An ActionResult for returning RSS feeds.
+    /// </summary>
+    /// <remarks>
+    /// Refer to https://github.com/benfoster/Fabrik.Common/blob/dev/src/Fabrik.Common.Web/ActionResults/RssFeedResult.cs.
+    /// </remarks>
+    public class RssFeedResult : FeedResult
+    {
+        public RssFeedResult(SyndicationFeed feed)
+            : base(new Rss20FeedFormatter(feed), "application/rss+xml")
+        {
+        }
+    }
+}
