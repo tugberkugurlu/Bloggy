50affde06db1f0914de56833ffa9396c192c65a2
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-10-27 15:08:59 +0200

    added BlogPosts_WithCommentsCount AbstractTransformerCreationTask

---
+++ src/Bloggy.Domain/Indexes/BlogPosts_WithCommentsCount.cs
@@ -0,0 +1,27 @@
+﻿using Bloggy.Domain.Entities;
+using Raven.Client.Indexes;
+using System.Linq;
+
+namespace Bloggy.Domain.Indexes
+{
+    public class BlogPosts_WithCommentsCount : AbstractTransformerCreationTask<BlogPostComment>
+    {
+        public class TransformeredResult
+        {
+            public BlogPost BlogPost { get; set; }
+            public int CommentsCount { get; set; }
+        }
+
+        public BlogPosts_WithCommentsCount()
+        {
+            TransformResults = blogPostComments => from comment in blogPostComments
+                                                   where comment.IsSpam == false && comment.IsApproved == true
+                                                   group comment by comment.BlogPostId into groupedResult
+                                                   select new
+                                                   {
+                                                       BlogPost = LoadDocument<BlogPost>(groupedResult.Key),
+                                                       CommentsCount = groupedResult.Count()
+                                                   };
+        }
+    }
+}
