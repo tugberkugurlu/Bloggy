cab5f71839e406ce7be2d5073df09a83cffe535a
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-10-27 18:24:06 +0200

    adding api controllers

---
+++ src/Bloggy.Client.Web/Controllers/AtomPub/RavenApiController.cs
@@ -0,0 +1,27 @@
+﻿using Raven.Client;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Bloggy.Client.Web.Controllers.AtomPub
+{
+    public abstract class RavenApiController : ApiController
+    {
+        protected readonly IAsyncDocumentSession RavenSession;
+
+        protected RavenApiController(IAsyncDocumentSession documentSession)
+        {
+            RavenSession = documentSession;
+        }
+
+        public async override Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = await base.ExecuteAsync(controllerContext, cancellationToken);
+            await RavenSession.SaveChangesAsync();
+
+            return response;
+        }
+    }
+}
