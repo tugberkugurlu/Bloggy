ad354fe55380f083e52d0cfc0f547a1a0c2f19de
gokhangulbiz <ggulbiz@gmail.com>
2013-10-27 15:12:28 +0200

    Null reference fix for tags.

--- src/Bloggy.Client.Web/Models/BlogPostModelLight.cs
+++ src/Bloggy.Client.Web/Models/BlogPostModelLight.cs
@@ -10,11 +10,16 @@ namespace Bloggy.Client.Web.Models
         public string Language { get; set; }
         public string BriefInfo { get; set; }
         public string Content { get; set; }
-        public TagModel[] Tags { get; set; }
+        public List<TagModel> Tags { get; set; }
         public string Slug { get; set; }
         public bool AllowComments { get; set; }
         public bool IsApproved { get; set; }
         public DateTimeOffset CreatedOn { get; set; }
         public DateTimeOffset LastUpdatedOn { get; set; }
+
+        public BlogPostModelLight()
+        {
+            Tags = new List<TagModel>();
+        }
     }
 }
